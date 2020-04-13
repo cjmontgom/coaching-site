b7526745afa0561dabcf1ee4d35948993039694f
Chloe Montgomery <chloemontgomery01@gmail.com>
2020-04-13 11:47:38 +0100

    Revert and simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 
     // webpack will take the files from ./src/client/index
-    entry: './src/index',
+    entry: './src/index.tsx',
 
     // and output it into /dist as bundle.js
     output: {
@@ -12,14 +12,9 @@ module.exports = {
     },
 
     // specify localhost port to listen on (this is not required, webpack-dev-server will run portfinder)
-    // proxy is for when you have a separate API backend development server and we want to send API requests on the same domain.
-    // In our case, we have a Node.js/Express backend where we want to send the API requests to.
     devServer: {
         port: 9000,
-        open: true,
-        proxy: {
-            "/api": "http://localhost:8080"
-        }
+        open: true
     },
 
     // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
