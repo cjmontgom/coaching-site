81e7a27b6eccc0e9964713ce3481118bf244737f
Chloe Montgomery <chloemontgomery01@gmail.com>
2020-04-17 15:48:46 +0100

    fix server

--- server.ts
+++ server.ts
@@ -14,11 +14,11 @@ const mockResponse = {
 app.use(express.static(DIST_DIR));
 
 // a route to test the server is running properly
-app.get('/api', (_req: any, res: any) => {
+app.get('/api', (req: any, res: any) => {
     res.send(mockResponse);
 });
 
-app.get('*', (_req: any, res: any) => {
+app.get('*', (req: any, res: any) => {
     res.sendFile(HTML_FILE);
 });
 
